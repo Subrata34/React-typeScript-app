33f8e3eed50dd24123a4933fd7e3ba15cee6b9d2
Subrata34 <subratamodak3491@gmail.com>
2022-07-04 22:34:21 +0600

    files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import './App.css';
+import User from './Components/User';
 
 function App() {
   return (
     <div className="App">
-      <h1>React typescript</h1>
+      <h1>User Mangment</h1>
+      <User name="Subrata Modak"></User>
     </div>
   );
 }
--- src/Components/User.tsx
+++ src/Components/User.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const User = () => {
+const User = (props:{name:string}) => {
   return (
-    <div>User</div>
+    <div>{props.name}</div>
   )
 }
 
